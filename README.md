b68ba9c556f3f3885607c09fad1dc0b93e8d82d4
takahashi <hisashi2223@gmail.com>
2025-03-06 15:19:37 +0900

    ORISの実装，INITだけ

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Maximum Walking Distance:乗客の最大徒歩移動距離  
-Transit point:マーカーの種類を選択(使わない)  
 Traveling route:巡回経路を表示  
 Share-Ride route:乗客の移動を考慮した相乗り経路を表示(1:従来手法，2:乗客の余分な移動を排除)
--- pathSearch.py
+++ pathSearch.py
@@ -5,6 +5,8 @@ import math
 import networkx as nx
 import pyproj
 import db
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 
 #クリックされた点郡を含む最小の矩形領域
 def rectangleArea(points):
@@ -329,3 +331,119 @@ def sharedRidePath(points, link, length, moveDist, value, len_dic):
     # print(len(positions_SRP))
 
     return path, length_SRP, points_SRP, positions_SRP_a, path_positions, len_walk
+
+#########################################################################################
+def init_vehicle(G, Q, st, len_dic):
+    d = {}
+    pie = {}
+    T = {}
+    for node in G.nodes:
+        d[node] = float('inf')
+        pie[node] = None
+        T[node] = None
+    d[st] = 0
+    for q in Q:
+        d[st] += len_SP(G, q[0], st, len_dic) + len_SP(G, q[1], st, len_dic)
+    T[st] = {(st, 0)}
+    for q in Q:
+        T[st].add((q[0],len_SP(G, q[0], st, len_dic)))
+        T[st].add((q[1],len_SP(G, q[1], st, len_dic)))
+    return d, pie, T
+
+def ORIS(G, Q, st, en, moveDist, len_dic):
+    d, pie, T = init_vehicle(G, Q, st, len_dic)
+
+    return
+
+#########################################################################################
+def create_data_model(points, link, length, len_dic):
+    G = linkToGraph(link, length)
+    connectGraph(G)
+    positions = []
+    for p in points:
+        positions.append(str(nearestNode(p, link)))
+
+    data = {}
+    DM = []
+    for p in positions:
+        d = []
+        for q in positions:
+            d.append(len_SP(G, p, q, len_dic))
+        DM.append(d)
+
+    data["distance_matrix"] = DM
+    data["num_vehicles"] = 4
+    data["depot"] = 0
+
+    return data
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    max_route_distance = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += f" {manager.IndexToNode(index)} -> "
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        plan_output += f"{manager.IndexToNode(index)}\n"
+        plan_output += f"Distance of the route: {route_distance}m\n"
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print(f"Maximum of the route distances: {max_route_distance}m")
+
+def mTSP(points, link, length, len_dic):
+    data = create_data_model(points, link, length, len_dic)
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+    )
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["distance_matrix"][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    dimension_name = "Distance"
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name,
+    )
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+    else:
+        print("No solution found !")
